75b425dbdae01bd613f0cfb5595655c40793ac30
CuberaArtem12 <kisila844@gmail.com>
2026-05-17 21:07:43 +0300

    add 9 lb one task

---
+++ My project C#/lb9/lb9/MyStruct/Birthday.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb9.MyStruct
+{
+    struct Birthday
+    {
+        public DateOnly birthDay { get; set; }
+        private int afteryear;
+        public int AfterYear
+        {
+            get { return afteryear; }
+            set {
+                if (value <= birthDay.Year) throw new ArgumentOutOfRangeException("value must be greater than year of birth");
+                afteryear = value;
+            }
+        }
+        public Birthday(DateOnly birthDay) {
+            this.birthDay = birthDay;
+        }
+        public DayOfWeek GetBirthDayOfWeek()
+        {
+            return birthDay.DayOfWeek;
+        }
+        public DayOfWeek GetDayOfWeekInYear() {
+            DateOnly DayOfWeekInYear = new DateOnly(AfterYear, birthDay.Month, birthDay.Day);
+            return DayOfWeekInYear.DayOfWeek;
+        }
+        public int daysUntilBirthday(){
+            DateOnly todaydate = DateOnly.FromDateTime(DateTime.Now);
+            DateOnly BirthDayNext = new DateOnly(todaydate.Year, birthDay.Month, birthDay.Day);
+            if (BirthDayNext <= todaydate) {
+               BirthDayNext = new DateOnly(todaydate.Year+1, birthDay.Month, birthDay.Day);
+            }
+            return BirthDayNext.DayNumber - todaydate.DayNumber;
+        }
+    }
+}
---
+++ My project C#/lb9/lb9/Program.cs
@@ -0,0 +1,21 @@
+﻿using lb9.MyStruct;
+
+namespace lb9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Birthday db=new Birthday();
+            int year = 2008;
+            int day = 4;
+            int month = 7;
+
+            db.birthDay=new DateOnly(2008, 7, 4);
+            Console.WriteLine($"Day of week: {db.GetBirthDayOfWeek()}");
+            db.AfterYear = 2030;
+            Console.WriteLine($"Nex Birthday: {db.GetDayOfWeekInYear()}");
+            Console.WriteLine($"days Until Birthday: {db.daysUntilBirthday()}");
+        }
+    }
+}
